ccae7412400a98137a1e2452bd58a5d801db1627
Olivier LDff <olivier.ldff@gmail.com>
2021-02-02 22:04:57 +0100

    ✨ Support jsonrpc v2.0 with answer

--- JsonRPC.h
+++ JsonRPC.h
@@ -4,27 +4,38 @@
 #include "Arduino.h"
 #include "aJSON.h"
 
+#define JSONRPC_MAX_FUNCTION_NAME 32
+
+#define JSONRPC_PARSE_ERROR -32700
+#define JSONRPC_INVALID_REQUEST -32600
+#define JSONRPC_METHOD_NOT_FOUND -32601
+#define JSONRPC_INVALID_PARAM -32602
+#define JSONRPC_INTERNAL_ERROR -32603
+
 struct Mapping
 {
-    String name;
-    void (*callback)(aJsonObject*);
+  char name[JSONRPC_MAX_FUNCTION_NAME];
+  void (*callback)(const aJsonObject*, aJsonObject*);
 };
 
 struct FuncMap
 {
-    Mapping* mappings;
-    unsigned int capacity;
-    unsigned int used;
+  Mapping* mappings;
+  unsigned int capacity;
+  unsigned int used;
 };
 
 class JsonRPC
 {
-    public:
-	JsonRPC(int capacity);
-	void registerMethod(String methodname, void(*callback)(aJsonObject*));
-	void processMessage(aJsonObject *msg);
-    private:
-	FuncMap* mymap;
+public:
+  JsonRPC(int capacity);
+  void registerMethod(const char* methodname, void (*callback)(const aJsonObject*, aJsonObject*));
+  bool processMessage(const aJsonObject* msg, aJsonObject* result);
+
+  static void writeError(aJsonObject* result, int errorCode);
+
+private:
+  FuncMap* mymap;
 };
 
 #endif
